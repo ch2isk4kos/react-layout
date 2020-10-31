ed583f06bb0bc0227cdb00818f948f9687426d72
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-31 19:59:23 -0400

    build navigation component

--- layout-client/src/components/Navigation.js
+++ layout-client/src/components/Navigation.js
@@ -1,10 +1,63 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { links, social } from '../data/links.js';
+import { FaBars } from 'react-icons/fa';
 
 const Navigation = () => {
+  const [ link, setLink ] = useState(false);
+  const linkContainerRef = useRef(null);
+  const linkRef = useRef(null);
+  const linkToggle = () => {
+    setLink(!link);
+  };
+
+  useEffect(() => {
+    const linkHeight = linkRef.current.getBoundingClientRect().height;
+    if (link) {
+      linkContainerRef.current.style.height = `${linkHeight}px`;
+    } else {
+      linkContainerRef.current.style.height = '0px';
+    }
+  }, [link]); 
+
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div className='Navigation'>
+        <div className='brand'>
+          <img className='logo' src={""} alt='logo' />
+          <button className='toggle' onClick={linkToggle}>
+            <FaBars />
+          </button>
+        </div>
+        <div className='link-containter' ref={linkContainerRef}>
+          <ul className='links' ref={linkRef}>
+            {
+              links.map(li => {
+                const { id, url, text } = li;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })
+            };
+          </ul>
+        </div>
+        <div className='social'>
+          <ul className='icons'>
+            {
+              social.map((i) => {
+                const { id, url, icon } = i;
+                return (
+                  <li key={id}>
+                    <a href={url}>{icon}</a>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
