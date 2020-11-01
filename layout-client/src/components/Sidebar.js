437f1a36ecccd035bc2bdf78e0922bf1579ed3c7
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-01 00:40:53 -0400

    import data and render sidebar component

---
+++ layout-client/src/components/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useGlobalContext } from '../data/context';
+import { sideSocial, sideLinks } from '../data/links';
+import { FaTimes } from 'react-icons/fa';
+
+const Sidebar = () => {
+  const { isSideOpen, closeSide } = useGlobalContext();
+
+  return (
+    <aside className={`${isSideOpen ? 'Sidebar show-sidebar' : 'Sidebar'}`}>
+      <div className='sidebar-header'>
+        <img className='logo' src={''} alt='logo' />
+        <button className='close-btn' onClick={closeSide}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='side-links'>
+        {
+          sideLinks.map((li) => {
+            const { id, url, text, icon } = li;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}{text}</a>
+              </li>
+            )
+          })
+        }
+      </ul>
+      <ul className='side-social'>
+        {
+          sideSocial.map((li) => {
+            const { id, url, icon } = li;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
