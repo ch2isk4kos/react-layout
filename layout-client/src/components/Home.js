43c57145d6d1012308b0617959ea9cd5dfc6701f
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-31 21:57:43 -0400

    create home component and add functional template

---
+++ layout-client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  const { opeeSidebar, openModal } = useGlobalContext();
+  return (
+    <div className='Home'>
+      <main>
+        <button>
+
+        </button>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
